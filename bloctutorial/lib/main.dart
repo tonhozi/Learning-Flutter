04e865d45511a7610b0274d67bb8e8053d560b2f
Pedro Tonhozi de Oliveira <tonhozi@gmail.com>
2021-09-24 19:42:41 -0300

    decrement function

--- bloctutorial/lib/main.dart
+++ bloctutorial/lib/main.dart
@@ -52,12 +52,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
     });
   }
 
@@ -117,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
             width: 10,
           ),
           FloatingActionButton(
-            onPressed: _incrementCounter,
+            onPressed: _decrementCounter,
             tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
