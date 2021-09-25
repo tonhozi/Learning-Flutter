9d77b75ed4430f3a087c4dfbc93309f2cfed04d0
Pedro Tonhozi de Oliveira <tonhozi@gmail.com>
2021-09-25 08:48:31 -0300

    Implements models

---
+++ clean_architecture_tdd_course/test/features/number_trivia/data/model/number_trivial_model_test.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'Test text');
+
+  test(
+    'should be a subclass of NumberTrivia entity',
+    () async {
+      //assert
+      expect(tNumberTriviaModel, isA<NumberTrivia>());
+    },
+  );
+
+  group(
+    'fromJson',
+    () {
+      test('should return a valid model when the JSON number is an integer',
+          () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, equals(tNumberTriviaModel));
+      });
+
+      test(
+          'should return a valid model when the JSON number is regarded as a double',
+          () async {
+        // arrange
+        final Map<String, dynamic> jsonMap =
+            json.decode(fixture('trivia_double.json'));
+        // act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        // assert
+        expect(result, equals(tNumberTriviaModel));
+      });
+    },
+  );
+
+  group('toJson', () {
+    test('Should return a JSON map containing the proper data', () async {
+      // arrange
+      final expectedMap = {
+        "text": "Test text",
+        "number": 1,
+      };
+
+      // act
+      final result = tNumberTriviaModel.toJson();
+      // assert
+      expect(result, expectedMap);
+    });
+  });
+}
